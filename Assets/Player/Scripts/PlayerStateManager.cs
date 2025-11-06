b76db6d2cc9e19cc8507370df1687d4e0f27c0bd
Alyx Zael <98592893+AlqZdevFox@users.noreply.github.com>
2025-11-06 17:49:30 +0100

    Update PlayerStateManager.cs
    - Global State Attributes made public, for easier management.
    - Made Update Method to run any UpdateState the current state has.
    - Added SwitchState method, pretty self explainatory.

--- Assets/Player/Scripts/PlayerStateManager.cs
+++ Assets/Player/Scripts/PlayerStateManager.cs
@@ -9,10 +9,10 @@ public class PlayerStateManager : MonoBehaviour
 
     //Each unique Player State gets its variable, instantiated from the respective Script yadda yadda
     //NOTE: These are exemplary states, we can flesh out or change/delete any state for experimentation's sake
-    PlayerDamagedState DamagedState = new PlayerDamagedState();
-    PlayerFallingState FallingState = new PlayerFallingState();
-    PlayerIdleState IdleState = new PlayerIdleState();
-    PlayerJumpingState JumpingState = new PlayerJumpingState();
+    public PlayerDamagedState DamagedState = new PlayerDamagedState();
+    public PlayerFallingState FallingState = new PlayerFallingState();
+    public PlayerIdleState IdleState = new PlayerIdleState();
+    public PlayerJumpingState JumpingState = new PlayerJumpingState();
 
     #endregion
 
@@ -29,8 +29,22 @@ public class PlayerStateManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        //Calls whatever state's UpdateState method every frame
+        //No, UpdateState in each independent state does NOT update every frame, but THIS script's Update does, it's an Unity method!
+        currentState.UpdateState(this);
     }//EndOf Unity method Update
-    
+
+    #endregion
+
+    #region Own Methods
+    public void SwitchState(PlayerBaseState playerState)
+    {
+        //This method means to give the ability to change states, from one to another
+        //Up above you see we start with Idle State, but the player moves, interacts and does stuff
+        //Can't have much of a game if the player JUST idles, right?
+        currentState = playerState; //set current state as the one passed as a parameter, whichever it may be
+        playerState.EnterState(this); //TODO: Get back to this in the video
+    }//EndOf method SwitchState
+
     #endregion
 }
